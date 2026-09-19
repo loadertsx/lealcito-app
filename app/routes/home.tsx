5509b108e5cde2fcf2a20175beacbea4cc8ae671
Loadertsx <nahueldevelop@gmail.com>
2026-09-18 23:05:06 -0300

    add biome

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,19 +1,18 @@
 import { env } from "cloudflare:workers";
-
-import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+	return [
+		{ title: "New React Router App" },
+		{ name: "description", content: "Welcome to React Router!" },
+	];
 }
 
 export function loader() {
-  return { message: env.VALUE_FROM_CLOUDFLARE };
+	return { message: env.VALUE_FROM_CLOUDFLARE };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+	return <Welcome message={loaderData.message} />;
 }
